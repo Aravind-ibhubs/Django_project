d03f57d39958a88e858e60fd25036f99c779d1e1
Aravind-ibhubs <aravindyedida402@gmail.com>
2023-11-04 10:54:05 +0530

    testing -2

--- eduTech/syallabus/models.py
+++ eduTech/syallabus/models.py
@@ -6,3 +6,12 @@ class Books(models.Model):
     author = models.CharField(max_length=230)
     published = models.DateField(default= django.utils.timezone.now)
     cost = models.IntegerField(null=True)
+
+class Member(models.Model):
+    firstname = models.TextField(max_length=200)
+    lastname = models.TextField(max_length=200)
+    phone = models.IntegerField(null=True)
+    
+    def __str__(self):
+        return f"{firstname} {lastname}"
+    
